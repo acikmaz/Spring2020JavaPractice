6b0a962e627485675f5451e9043e10390ae298da
Ali Cikmaz <acikmaz19@gmail.com>
2020-04-23 10:49:36 -0500

    first commit

--- src/day27_DateTime/Dates.java
+++ src/day27_DateTime/Dates.java
@@ -12,5 +12,40 @@ public class Dates {
         LocalDate birthday = LocalDate.of(2000, 12, 25);
 
         System.out.println(birthday);
+
+        //    isAfter
+
+        boolean result1 = date1.isAfter(birthday);
+
+        System.out.println(result1);
+
+        // isBefore
+
+        boolean result2 = date1.isBefore(birthday);
+
+        System.out.println(result2);
+
+        // isEqual
+
+        boolean result3 = date1.isEqual(birthday);
+        System.out.println(result3);
+
+        // isLeapYear
+
+        boolean result4 = birthday.isLeapYear();
+        System.out.println(result4);
+
+        System.out.println("===============================================");
+
+        LocalDate now = LocalDate.now(); //2020-04-23
+
+        System.out.println("Today is: " + now);
+
+        String str = now.toString();
+
+        System.out.println(str);
+
+
+
     }
 }
