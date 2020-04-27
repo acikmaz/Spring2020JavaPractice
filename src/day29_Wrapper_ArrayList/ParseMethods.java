05332d56623cd34f20e6d74d82989360522504e3
acikmaz <acikmaz19@gmail.com>
2020-04-27 10:51:56 -0500

    add day29 class

--- src/day29_Wrapper_ArrayList/ParseMethods.java
+++ src/day29_Wrapper_ArrayList/ParseMethods.java
@@ -21,6 +21,42 @@ public class ParseMethods {
         //Integer = (int) byte
         //Integer = int
 
+        String str2 = "10.5";
+
+        float f1 = Float.parseFloat( str2 ); // float 10.5
+
+        System.out.println(f1 + 1);
+
+        double d1 = Double.parseDouble( str2 ); // double 10.5
+
+        System.out.println(d1 + 1);
+
+
+        String str3 = "3147483647";
+
+        long num1 = Long.parseLong(str3); //3147483647L //it can be assigned double & float
+
+        System.out.println(num1 + 2);
+
+        Long num2 = Long.parseLong(str3);
+
+        System.out.println(num2 + 2);
+
+        String result1 = "tRUe";  //any string other than "true" returns "false"
+
+                        // non cases sensitive to "true and false"
+
+        boolean r1 = Boolean.parseBoolean(result1); // boolean true
+
+        System.out.println(!r1); // false
+
+        String result2 = "FalSe"; // false
+
+        boolean r2 = Boolean.parseBoolean(result2);
+        System.out.println(r2);
+
+
+
 
 
 
