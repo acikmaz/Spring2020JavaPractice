aa7a376494fad0f42518053482ece0084a1201ea
acikmaz <acikmaz19@gmail.com>
2020-04-27 09:29:42 -0500

    day29 Class

---
+++ src/day29_Wrapper_ArrayList/WrapperClasss.java
@@ -0,0 +1,36 @@
+package day29_Wrapper_ArrayList;
+
+public class WrapperClasss {
+    public static void main(String[] args) {
+
+        int a = 100; //primitive
+
+        Integer b = a; //Wrapper class
+
+        short s1 = 200;
+
+        int n1 = s1;
+
+        Integer N1 = n1; //does not accept other primitives, dedicated only its primitive
+
+        //==============
+
+        int a1 = 20;
+        float f1 = a1;
+
+        Float f2 = f1;
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
