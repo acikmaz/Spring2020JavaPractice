0cbe5193606320dce236dcea9ee9b343ca8ae33c
acikmaz <acikmaz19@gmail.com>
2020-04-27 09:33:13 -0500

    update day29 class

--- src/day29_Wrapper_ArrayList/WrapperClasss.java
+++ src/day29_Wrapper_ArrayList/WrapperClasss.java
@@ -3,23 +3,31 @@ package day29_Wrapper_ArrayList;
 public class WrapperClasss {
     public static void main(String[] args) {
 
-        int a = 100; //primitive
+        int a = 100;  // primitive
 
-        Integer b = a; //Wrapper class
+        Integer b = a;  // wrapper class
 
         short s1 = 200;
-
         int n1 = s1;
 
-        Integer N1 = n1; //does not accept other primitives, dedicated only its primitive
+        // Integer N1 = s1;  // Integer is dedicated to int primitive
+        Integer n2 = n1;
 
-        //==============
+
+        //============================
 
         int a1 = 20;
         float f1 = a1;
 
-        Float f2 = f1;
+        //    Float f2 = a1;
 
+        Integer I1 = 200;
+        int a2 = I1;
+
+        Byte B1 = 126;
+        int a3 = B1;
+
+        System.out.println(a3);
 
 
 
