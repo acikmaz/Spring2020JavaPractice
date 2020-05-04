bc12c726c9ca882cbf26d848a2b4d91ab37f4984
acikmaz <acikmaz19@gmail.com>
2020-05-04 11:14:12 -0500

    add day33 Class

---
+++ src/day33_CustomClass/Car.java
@@ -0,0 +1,23 @@
+package day33_CustomClass;
+
+import sun.jvm.hotspot.oops.BranchData;
+
+public class Car {
+
+    String Brand;
+    String model;
+    int year;
+    String color;
+
+    public static void main(String[] args) {
+        int a = 100;
+
+
+
+
+
+
+
+
+    }
+}
